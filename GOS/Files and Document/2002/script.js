f48bc9ffa7e6ddbfdad800101ee1dc3b27744c8b
RajeshGovindan02 <113298736+RajeshGovindan02@users.noreply.github.com>
2022-12-04 11:10:39 +0530

    Add files via upload

---
+++ GOS/Files and Document/2002/script.js
@@ -0,0 +1,4 @@
+function showText() {
+	document.getElementById("label").innerHTML="Successfully Created 2002 Project";
+	document.getElementById("btn").style="background-color:green;"
+}
---
+++ GOS/Files and Document/Rajesh first project/script.js
@@ -0,0 +1,4 @@
+function showText() {
+	document.getElementById("label").innerHTML="Successfully Created Rajesh first project Project";
+	document.getElementById("btn").style="background-color:green;"
+}
